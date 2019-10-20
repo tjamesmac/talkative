f4833abde2f1ffc6848c13dd8aaabe77dda0cd08
tjamesmac <tjamesmac@gmail.com>
2019-10-20 14:13:12 +0100

    Add formdata

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <form v-on:submit="postFile">
       <label>
         Upload file here:
-         <input id="fileUpload" type="file" />
+         <input id="fileUpload" name="fileUpload" type="file" />
       </label>
       <input type="submit" />
     </form>
@@ -13,28 +13,34 @@
 <script>
 export default {
   name: 'HelloWorld',
+  data: function () {   //eslint-disable-line
+    return {
+      count: 0,
+    };
+  },
   props: {
     msg: String,
   },
   methods: {
     postFile: async (event) => {
       event.preventDefault();
-      console.log('hello world');
       const fileUpload = document.getElementById('fileUpload');
-      console.log(fileUpload.value);
-      console.log(fileUpload.files[0]);
+      const formData = new FormData();
+      formData.append('streamfile', fileUpload.files[0]);
       try {
         const dataUpload = await fetch('/', {
           method: 'POST',
           headers: {
             Accept: 'application/json',
-            'Content-Type': 'application/json',
           },
-          body: fileUpload.files[0],
+          body: formData,
         });
         const response = await dataUpload;
         if (response.status === 200) {
           console.log('yay');
+          const responseJSON = await response.json();
+
+          console.log(responseJSON);
         }
       } catch (error) {
         console.error('File upload error', error);
